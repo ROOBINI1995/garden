bb77d01288469e44803f6c83d5ed5cec1147f9a6
ROOBINI1995 <roobini388@gmail.com>
2016-07-10 12:16:37 +0530

    Update and rename README.md to README.C

---
+++ README.C
@@ -0,0 +1,21 @@
+# garden
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,c,;
+int i,r;
+int sum=1;
+int digit;
+printf(""enter a number n:");
+scanf("%d",&n);
+for(i=1;i<=2n;i++)
+{
+digit=n%10;
+sum=sum+n;
+r=n%10;
+c=2*n*2*3.14*r;
+printf("circumference of a 2n table:",c);
+getch();
+}
+}
